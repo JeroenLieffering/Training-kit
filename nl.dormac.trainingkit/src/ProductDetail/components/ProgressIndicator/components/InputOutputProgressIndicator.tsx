7de1c51efa3da0591899531c684e66ca3e8a6fdf
Jeroen <j.lieffering@dormac.nl>
2026-01-15 15:08:38 +0100

    Create InputOutputProgressIndicator.tsx

---
+++ nl.dormac.trainingkit/src/ProductDetail/components/ProgressIndicator/components/InputOutputProgressIndicator.tsx
@@ -0,0 +1,183 @@
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+import AirIcon from '@mui/icons-material/AirOutlined';
+import BackHandIcon from '@mui/icons-material/BackHandOutlined';
+import GridOnIcon from '@mui/icons-material/GridOn';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import OutputIcon from '@mui/icons-material/Output';
+import PrecisionManufacturingIcon from '@mui/icons-material/PrecisionManufacturingOutlined';
+import ShowerOutlinedIcon from '@mui/icons-material/ShowerOutlined';
+import StorageIcon from '@mui/icons-material/Storage';
+import React, { useRef } from 'react';
+import { CobotConfig } from '../../../../core/models/cobot-config-types';
+import { Product } from '../../../../core/models/product-types';
+import { MetaSuiteProgramState } from '../../../../drl/main/types';
+import { ProgressIcon } from '../../ProgressIcon';
+import { getProgressStatusForPhase } from '../utils';
+import { isConfiguredWithSupportForMultipleDrawers } from '../../../../core/services/cobot-config-service';
+
+type Props = {
+  product: Product;
+  cobotConfig: CobotConfig;
+  status: MetaSuiteProgramState;
+  isPlaying: boolean;
+};
+
+const HOME = 0;
+const DRAWER = 1;
+const PICK = 2;
+const AIRPURGE_BEFORE_INFEED = 3;
+const HAND = 4;
+const AIRPURGE_BEFORE_OUTFEED = 5;
+const OUTPUT = 6;
+const AIRPURGE_AFTER_OUTFEED = 7;
+const CLEAN = 8;
+const GRID = 9;
+
+const PHASES: MetaSuiteProgramState[][] = [
+  // HOME
+  ['HOMING', 'ROBOT_HOME'],
+  // DRAWER
+  ['REQUEST_NEW_DRAWER'],
+  // PICK
+  [
+    'PICK_PRODUCT_DRAWER',
+    'PRODUCT_PICKED_FROM_DRAWER',
+    'PICK_FROM_DRAWER_FAILED',
+  ],
+  // AIRPURGE_BEFORE_INFEED,
+  ['AIRPURGE_BEFORE_INFEED', 'AIRPURGE_BEFORE_INFEED_DONE'],
+  // HAND
+  ['PLACE_PRODUCT_MACHINE', 'FORCE_FEED', 'PUSH_AFTER_PLACE'],
+  // AIRPURGE_BEFORE_OUTFEED,
+  ['AIRPURGE_BEFORE_OUTFEED', 'AIRPURGE_BEFORE_OUTFEED_DONE'],
+  // OUTPUT,
+  [
+    'PICK_FINISHED_PRODUCT',
+    'PRODUCT_PICKED_FROM_MACHINE',
+    'PICK_FROM_MACHINE_FAILED',
+  ],
+  // AIRPURGE_AFTER_OUTFEED,
+  ['AIRPURGE_AFTER_OUTFEED', 'AIRPURGE_AFTER_OUTFEED_DONE'],
+  // CLEAN
+  ['CLEAN_PRODUCT', 'PRODUCT_CLEANED'],
+  // GRID / GPS
+  [
+    'PLACE_FINISHED_PRODUCT',
+    'PLACE_PRODUCT_DRAWER',
+    'PRODUCT_FINISHED',
+    'TOTAL_FINISHED',
+  ],
+];
+
+export function InputOutputProgressIndicator({
+  product,
+  status,
+  isPlaying,
+  cobotConfig,
+}: Props) {
+  const lastPhase = useRef(0);
+
+  if (status === 'PICK_FROM_DRAWER_FAILED') {
+    lastPhase.current = 1;
+  }
+
+  let phase = PHASES.findIndex((phases) => phases.includes(status));
+
+  if (phase === -1) {
+    // If no phase can be found default to the last known phase
+    // so the progress animation always shows one item in blue.
+    phase = lastPhase.current;
+  } else {
+    // Set last known phase.
+    lastPhase.current = phase;
+  }
+
+  return (
+    <div className="tw-flex tw-items-center">
+      <ProgressIcon
+        icon={HomeOutlinedIcon}
+        status={getProgressStatusForPhase(HOME, phase, isPlaying)}
+      />
+
+      {isConfiguredWithSupportForMultipleDrawers(cobotConfig) ? (
+        <ProgressIcon
+          icon={StorageIcon}
+          status={getProgressStatusForPhase(DRAWER, phase, isPlaying)}
+        />
+      ) : null}
+
+      <ProgressIcon
+        icon={PrecisionManufacturingIcon}
+        status={
+          status === 'PICK_FROM_DRAWER_FAILED'
+            ? 'error'
+            : getProgressStatusForPhase(PICK, phase, isPlaying)
+        }
+      />
+
+      {product.config.AIRPURGE_BEFORE_INFEED ? (
+        <ProgressIcon
+          icon={AirIcon}
+          status={getProgressStatusForPhase(
+            AIRPURGE_BEFORE_INFEED,
+            phase,
+            isPlaying,
+          )}
+        />
+      ) : null}
+
+      <ProgressIcon
+        icon={BackHandIcon}
+        status={getProgressStatusForPhase(HAND, phase, isPlaying)}
+      />
+
+      {product.config.AIRPURGE_BEFORE_OUTFEED ? (
+        <ProgressIcon
+          icon={AirIcon}
+          status={getProgressStatusForPhase(
+            AIRPURGE_BEFORE_OUTFEED,
+            phase,
+            isPlaying,
+          )}
+        />
+      ) : null}
+
+      <ProgressIcon
+        icon={OutputIcon}
+        status={
+          status === 'PICK_FROM_MACHINE_FAILED'
+            ? 'error'
+            : getProgressStatusForPhase(OUTPUT, phase, isPlaying)
+        }
+      />
+
+      {product.config.AIRPURGE_AFTER_OUTFEED ? (
+        <ProgressIcon
+          icon={AirIcon}
+          status={getProgressStatusForPhase(
+            AIRPURGE_AFTER_OUTFEED,
+            phase,
+            isPlaying,
+          )}
+        />
+      ) : null}
+
+      {product.config.CLEAN_PRODUCT ? (
+        <ProgressIcon
+          icon={ShowerOutlinedIcon}
+          status={getProgressStatusForPhase(CLEAN, phase, isPlaying)}
+        />
+      ) : null}
+
+      <ProgressIcon
+        icon={
+          product.config.PLACE_FINISHED_PRODUCT_ON_DROP_OFF_POSITION
+            ? GpsFixedIcon
+            : GridOnIcon
+        }
+        status={getProgressStatusForPhase(GRID, phase, isPlaying)}
+        line={false}
+      />
+    </div>
+  );
+}
